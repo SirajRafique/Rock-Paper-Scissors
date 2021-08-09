480f0c32f9bed55edeb26094e1ebb30f901416b6
SirajRafique <sirajrafique@gmail.com>
2021-08-09 21:38:52 +0000

    add a new line character for input method to work correctly

--- run.py
+++ run.py
@@ -6,12 +6,12 @@ comp_score = 0
 """A loop to play again and again"""
 
 while True:
-    user_action = str(input("Enter your choice (rock, paper or scissors): "))
+    user_action = str(input("Enter your choice (rock, paper or scissors):\n"))
 
     """Check if user choice is valid or not"""
     while user_action != "rock" and user_action != "paper" \
             and user_action != "scissors":
-        user_action = str(input("Invalid choice, please enter again: "))
+        user_action = str(input("Invalid choice, please enter again:\n"))
 
     """Assign random choice to computer"""
     possible_actions = ["rock", "paper", "scissors"]
@@ -54,6 +54,6 @@ while True:
     print("Computer =", comp_score)
 
     """Play again or stop"""
-    play_again = input("Play again? (y/n): ")
+    play_again = input("Play again? (y/n):\n")
     if play_again.lower() != "y":
         break
